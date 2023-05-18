fc78b7a51a616202ab12c9d7859d47921139bc33
joncoe <info@joncoe.com>
2023-05-18 19:33:07 +0900

    Add Forum route and page component

---
+++ src/pages/Forum.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { defineProps, computed } from 'vue';
+import ThreadList from '@/components/ThreadList.vue';
+
+import sourceData from "@/data.json";
+
+const props = defineProps({
+  id: {
+    required: true,
+    type: String
+  }
+})
+
+const forum = computed(() => {
+  return sourceData.forums.find(forum => forum.id === props.id)
+})
+const threads = computed(() => {
+  return sourceData.threads.filter(thread => thread.forumId === props.id)
+})
+
+</script>
+<template>
+    <div class="col-full push-top">
+      <div class="forum-header">
+        <div class="forum-details">
+          <h1>Cooking</h1>
+          <p class="text-lead">Discuss your passion for food and cooking</p>
+        </div>
+        <a href="new-thread.html" class="btn-green btn-small">Start a thread</a>
+      </div>
+    </div>
+
+
+
+    <div class="col-full">
+
+      <ThreadList :threads="threads"/>
+
+    </div>
+
+</template>
+
+
+<style lang="scss" scoped></style>
