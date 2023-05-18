b4852c2c8a5758409577be2dace39565eb06447b
joncoe <info@joncoe.com>
2023-05-18 16:51:35 +0900

    Add a New Post form

--- src/pages/ThreadShow.vue
+++ src/pages/ThreadShow.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { reactive, computed, defineProps } from "vue";
+import { reactive, ref, computed, defineProps } from "vue";
 import sourceData from "@/data.json";
 import PostList from "@/components/PostList.vue";
 
 const threads = reactive(sourceData.threads);
 const posts = reactive(sourceData.posts);
+const newPostText = ref("")
 
 const props = defineProps(['id'])
 
@@ -16,6 +17,20 @@ const threadPosts = computed(() => {
   return posts.filter(post => post.threadId === props.id)
 })
 
+const addPost = () => {
+  const postId = 'ggqq' + Math.random()
+  const post = {
+    id: postId,
+    text: newPostText.value,
+    publishedAt: Math.floor(Date.now() / 1000),
+    threadId: props.id,
+    userId: 'rpbB8C6ifrYmNDufMERWfQUoa202'
+  }
+  posts.push(post)
+  thread.value.posts.push(postId)
+  newPostText.value = "";
+}
+
 </script>
 <template>
   <div class="col-large push-top">
@@ -23,10 +38,28 @@ const threadPosts = computed(() => {
 
     <PostList :posts="threadPosts" />
 
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <label for="thread_content">Content:</label>
+        <textarea
+          id="thread_content"
+          class="form-input"
+          name="content"
+          rows="8"
+          cols="140"
+          placeholder="Enter your post."
+          v-model="newPostText"
+        />
+      </div>
+
+      <div class="btn-group">
+        <button class="btn btn-ghost">Cancel</button>
+        <button class="btn btn-blue" type="submit" name="Publish">Publish </button>
+      </div>
+    </form>
+
   </div>
 </template>
 
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
