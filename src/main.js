d5fffbb8d4acf18bd947b788e93ef802ba6b59a6
joncoe <info@joncoe.com>
2023-05-19 17:39:15 +0900

    install the store into the app

--- src/main.js
+++ src/main.js
@@ -2,8 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue';
 import AppDate from '@/components/AppDate.vue'
 import router from '@/router';
+import store from '@/store';
 
 const forumApp = createApp(App);
 forumApp.use(router)
+forumApp.use(store)
 forumApp.component('AppDate', AppDate)
 forumApp.mount('#app');
