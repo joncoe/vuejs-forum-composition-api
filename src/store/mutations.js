3c09f570d99dbc29ef646dddeeeed57e838475b0
joncoe <info@joncoe.com>
2023-05-24 17:37:36 +0900

    Break apart store into separate files. Clean up Actions functions

---
+++ src/store/mutations.js
@@ -0,0 +1,38 @@
+import { upsert, findById } from "@/helpers";
+
+export default {
+  appendPostToThread: makeAppendChildToParentMutation({
+    parent: "threads",
+    child: "posts",
+  }),
+  appendThreadToForum: makeAppendChildToParentMutation({
+    parent: "forums",
+    child: "threads",
+  }),
+  appendThreadToUser: makeAppendChildToParentMutation({
+    parent: "users",
+    child: "posts",
+  }),
+  appendContributorToThread: makeAppendChildToParentMutation({
+    parent: "threads",
+    child: "contributors",
+  }),
+  setItem (state, { resource, item, log }) {
+    if (log) console.log('setItem', resource, item)
+    upsert(state[resource], item);
+  },
+}
+
+function makeAppendChildToParentMutation({ parent, child }) {
+  return (state, { childId, parentId }) => {
+    const resource = findById(state[parent], parentId); //
+    if (!resource) {
+      console.error(`⚠️ ⚠️ ⚠️ ⚠️ ${child} ${childId} to ${parent} ${parentId} failed because it doesn't exist`)
+      return
+    }
+    resource[child] = resource[child] || [];
+    if (!resource[child].includes(childId)) {
+      resource[child].push(childId);
+    }
+  };
+}
