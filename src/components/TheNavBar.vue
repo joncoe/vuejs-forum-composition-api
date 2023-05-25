469adc1c2225f59ec69d6511bf97b9e80c183c79
joncoe <info@joncoe.com>
2023-05-25 16:18:18 +0900

    Add navbar user dropdown toggle, links.

--- src/components/TheNavBar.vue
+++ src/components/TheNavBar.vue
@@ -1,10 +1,14 @@
 <script setup>
-import {computed} from 'vue';
+import {computed, ref} from 'vue';
 import { useStore } from 'vuex';
 
 const store = useStore();
-
+const userDropdownOpen = ref(false)
 const authUser = computed(() => store.getters.authUser)
+
+const toggle = () => {
+  userDropdownOpen.value = !userDropdownOpen.value;
+}
 
 </script>
 <template>
@@ -26,25 +30,24 @@ const authUser = computed(() => store.getters.authUser)
       <ul>
 
         <li v-if="authUser" class="navbar-user">
-          <router-link :to="{name: 'Profile'}">
+          <a @click.prevent="toggle">
             <img class="avatar-small" :src="authUser.avatar" :alt="`${authUser.name} profile picture`">
             <span>
               {{ authUser.name }}
               <img class="icon-profile" src="@/assets/svg/arrow-profile.svg" alt="">
             </span>
-          </router-link>
+          </a>
 
           <!-- dropdown menu -->
           <!-- add class "active-drop" to show the dropdown -->
-          <div id="user-dropdown">
+          <div id="user-dropdown" :class="{'active-drop': userDropdownOpen}">
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
-              <li class="dropdown-menu-item"><a href="profile.html">View profile</a></li>
-              <li class="dropdown-menu-item"><a href="#">Log out</a></li>
+              <li class="dropdown-menu-item"><router-link :to="{name: 'Profile'}">View profile</router-link></li>
+              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
             </ul>
           </div>
         </li>
-        <li v-if="authUser" class="navbar-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'SignIn'}">Sign In</router-link></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{name: 'Register'}">Register</router-link></li>
       </ul>
