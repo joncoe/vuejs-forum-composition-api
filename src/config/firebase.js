72a5a90d04e068f40a991f533181195a17ede070
joncoe <info@joncoe.com>
2023-05-25 13:39:14 +0900

    Update to use .env files

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,8 +1,8 @@
 module.exports = {
-  apiKey: "AIzaSyDLaZ1ECOzdCKdvSGnPt5xDS5NImLCQy20",
-  authDomain: "vueschool-forum-joncoe.firebaseapp.com",
-  projectId: "vueschool-forum-joncoe",
-  storageBucket: "vueschool-forum-joncoe.appspot.com",
-  messagingSenderId: "432931266037",
-  appId: "1:432931266037:web:493bf48296c94d884394bc"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
 }
